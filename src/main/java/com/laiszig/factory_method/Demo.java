76f6b765fc32bac4edcf582e0e99f32ce40f3b21
Lais_Ziegler <lais.ziegler@dell.com>
2023-04-26 13:58:31 -0300

    factory method

---
+++ src/main/java/com/laiszig/factory_method/Demo.java
@@ -0,0 +1,16 @@
+package com.laiszig.factory_method;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+class Point {
+    private double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
