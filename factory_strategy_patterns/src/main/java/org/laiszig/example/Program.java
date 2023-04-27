13562f66f02fb3f5f233f2fbad05ad76792fa9b5
Lais_Ziegler <lais.ziegler@dell.com>
2023-04-27 16:45:39 -0300

    add example and improved code with interface

--- factory_strategy_patterns/src/main/java/org/laiszig/example/Program.java
+++ factory_strategy_patterns/src/main/java/org/laiszig/example/Program.java
@@ -26,3 +26,13 @@ class Main {
     }
 }
 
+/*
+This implementation is valid, but it is not ideal because the class can become huge if we add more different types of shipping,
+and the same class will be changed every time we need to adjust or change any existing shipping way.
+
+It definitely can become a bottleneck in the future.
+
+In order to improve the code, we can start breaking the class into multiple other classes, and since they basically
+are only used to ship a product in different ways, we can create an interface, create a class for every kind of shipping,
+and ensure those classes implement the interface and implement the method to Ship the product
+ */
--- factory_strategy_patterns/src/main/java/org/laiszig/improved/Program.java
+++ factory_strategy_patterns/src/main/java/org/laiszig/improved/Program.java
@@ -38,4 +38,14 @@ class Main {
     }
 }
 
+/*
+Now, for every new type of shipping we just need to create a new class, implement the interface,
+and implement the Ship method, not having to change any other existing shipping class.
+
+If we need to change an existing shipping way we do not need to change the same big class anymore, which is great.
+
+But anyway, it still can be improved, imagine we want to avoid having to think about which class we should use,
+and instead of that we would prefer to have a component that handles all this creation process for us, in this case, we can use a factory.
+ */
+
 
